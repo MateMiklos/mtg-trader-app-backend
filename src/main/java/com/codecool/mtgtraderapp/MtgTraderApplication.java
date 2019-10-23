9bdaba0168d2d135bddb192b2941d58d0d0cbcc5
MateMiklos <miklos.mate.91@gmail.com>
2019-10-23 20:11:56 +0200

    project has been initialised

---
+++ src/main/java/com/codecool/mtgtraderapp/MtgTraderApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.mtgtraderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgTraderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtgTraderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/mtgtraderapp/MtgTraderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.mtgtraderapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtgTraderAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
